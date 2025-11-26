e32a2cdc6ce4207184b5b0ff3df017dc1f931136
tianming332 <jiangmingtian332@gmail.com>
2025-11-26 14:16:55 +0900

    优化8

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // data.js
 const base = 'https://tianming332.github.io/Tian-Video-Assets';
 const videoSources = [
-  `${base}/video_list_A.json`,
-  `${base}/video_list_B.json`,
+  `${base}/video_list_A.fixed.json`,
+  `${base}/video_list_B.fixed.json`,
 ];
